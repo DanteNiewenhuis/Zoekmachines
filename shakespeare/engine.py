1f3d1745fe63ac21470b56b0a7dc471af2bdf843
dante <d.niewenhuis@hotmail.com>
2017-09-10 23:48:18 +0200

    comments

--- shakespeare/engine.py
+++ shakespeare/engine.py
@@ -11,7 +11,7 @@ def count_tokens(folder):
     result = 0 # initialize Myindex
     for infile in os.listdir(folder):  # loop over each file
         with open(folder +'/'+ infile, 'r') as f:  # open file
-            for w in f:    # update Myindex with each token 
+            for w in f:     
                 result+=1
                 
     return result
@@ -27,16 +27,21 @@ def index_collection(folder):
 
     return Myindex
 
+# make a dict with the corpus frequency and the dictionary frequency 
+# for every word
 def frequency_index(Myindex):
     freq_index = {}
     for word in Myindex:
         result = 0
         for index in Myindex[word]:
             result += Myindex[word][index]
+
         freq_index[word]=[len(Myindex[word]),result] 
 
     return freq_index
 
+# return the amount of words that have the right the corpus frequency or 
+# dictionary frequency mode:0 will give the dictionary frequency and mode:1 the corpus. 
 def frequency_checker(index, amount, mode, greater=False):
     result = 0 
     if greater:
@@ -56,31 +61,33 @@ def corpus_frequency(freq_table):
     return result
 
 def opdracht1():
-    MyIndex = index_collection('tokens')
-    print(count_tokens['tokens']) # total tokens in the corpus
+    MyIndex = index_collection(folder)
+    print(count_tokens[folder]) # total tokens in the corpus
     print(len(MyIndex)) # total unique tokens in the corpus
 
 
-def opdracht5():
-    MyIndex = index_collection('tokens')
+def opdracht5(folder):
+    MyIndex = index_collection(folder)
     freq_table = frequency_index(MyIndex)
+
     print(len(freq_table))
     print(frequency_checker(freq_table, 1, 1))
     print(frequency_checker(freq_table, 1, 0))
-    print(frequency_checker(freq_table, len(os.listdir('tokens')), 0))
-    print(frequency_checker(freq_table, len(os.listdir('tokens'))/2, 0, greater=True))
+    print(frequency_checker(freq_table, len(os.listdir(folder)), 0))
+    print(frequency_checker(freq_table, len(os.listdir(folder))/2, 0, greater=True))
 
 
-def opdracht6():
-    MyIndex = index_collection('tokens')
+def opdracht6(folder):
+    MyIndex = index_collection(folder)
     freq_table = frequency_index(MyIndex)
-    corpus_freq = corpus_frequency(freq_table)
-    sort_freq = sorted(corpus_freq, reverse=True)
+    corpus_freq = corpus_frequency(freq_table) # get a list of frequencies of words in the corpus descending
+    sort_freq = sorted(corpus_freq, reverse=True) # sort the list
     x_axis = []
     for x in range(1,len(sort_freq)+1):
         x_axis.append(math.log(x))
 
+    #plot the graph
     plt.plot(x_axis, sort_freq)
     plt.show()   
 
-opdracht6()
+folder = 'tokens'
